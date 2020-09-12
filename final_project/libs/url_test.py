a4af457f5a7a3389169380a075fe88e54082ceed
jsjose <josem.sanjose@gmail.com>
2020-09-12 19:01:02 +0200

    requests

--- final_project/libs/url_test.py
+++ final_project/libs/url_test.py
@@ -1,4 +1,13 @@
-# url_test test url
+# url_test test url. Get weeb page. Basic link analysis and contents.
 
 import requests
 
+# User write their own threading code to drive the train_fn
+threads = []
+for _ in range(10):
+    p = threading.Thread(target=train_fn, args=())
+    p.start()
+    threads.append(p)
+
+for p in threads:
+    p.join()
